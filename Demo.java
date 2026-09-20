69f67e3ab7289edd463637b264a57184d5d5fe44
slaraibkamran <slaraibkamran.101@gmail.com>
2026-09-20 18:51:24 +0500

    Add files via upload

--- Demo.java
+++ Demo.java
@@ -4,30 +4,5 @@ public class Demo{
   s1.id = 128;
   s1.name = "Laraib";
   s1.completedCredits = 4;
-  
-  Student s2 = new Student();
-  s2.id = 129;
-  s2.name = "Batool";
-  s2.completedCredits = 3;
-  
-  System.out.println("Before changing info: ");
-  s1.printSummary();
-  s2.printSummary();
-
-  s1.name = "Falaq";
-  s1.completedCredits = 2;
-
-  System.out.println("After changing info: ");
-  s1.printSummary();
-  s2.printSummary();
-
-
-  System.out.println("New Function: ");
-  s1.addCredits(2);
-  s1.printSummary();
-  
-
-  
-
-  }
+  s1.printSummary();}
 }
--- Student.java
+++ Student.java
@@ -7,8 +7,4 @@ public class Student{
     System.out.println("ID: " + id);
     System.out.println("Name: " + name);
     System.out.println("Completed Credits: " + completedCredits);}
-
-    public int addCredits(int amount){
-    completedCredits += amount;
-    return completedCredits;}
 }
