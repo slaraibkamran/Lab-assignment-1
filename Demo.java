ba664f35ac451409e68f792636ebc5864e5179b4
slaraibkamran <slaraibkamran.101@gmail.com>
2026-09-20 18:53:54 +0500

    Add files via upload

--- Demo.java
+++ Demo.java
@@ -1,8 +1,26 @@
-public class Demo{ 
+public class Demo {
   public static void main (String[] args){
-  Student s1 = new Student();
-  s1.id = 128;
-  s1.name = "Laraib";
-  s1.completedCredits = 4;
-  s1.printSummary();}
-}
+  Student a = new Student();
+  a.name = "Ali";
+  Student b = a;
+  b.name = "Sara";
+  Student c = new Student();
+  c.name = a.name; 
+  
+  System.out.println("Before: ");
+  System.out.println("Name a: " + a.name);
+  System.out.println("Name b: " + b.name);
+  System.out.println("Name c: " + c.name);
+  System.out.println(a==b);
+  System.out.println(b==c);
+
+  b = new Student();
+  b.name = "Hira";
+
+  System.out.println("After: ");
+  System.out.println("Name a: " + a.name);
+  System.out.println("Name b: " + b.name);
+  System.out.println("Name c: " + c.name);
+
+  
+}}
