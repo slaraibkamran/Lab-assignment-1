347993a359cf8dbd20955733d8bc34521566cb51
slaraibkamran <slaraibkamran.101@gmail.com>
2026-09-20 18:45:42 +0500

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,8 @@
+public class Demo{ 
+  public static void main (String[] args){
+  Student s1 = new Student();
+  s1.id = 128;
+  s1.name = "Laraib";
+  s1.completedCredits = 4;
+  s1.printSummary();}
+}
---
+++ Student.java
@@ -0,0 +1,10 @@
+public class Student{
+    int id;
+    String name;
+    int completedCredits;
+
+    public void printSummary(){
+    System.out.println("ID: " + id);
+    System.out.println("Name: " + name);
+    System.out.println("Completed Credits: " + completedCredits);}
+}
